3d0b5577e4318b0c074c3d605e39fedec9d3adff
timDeHof <ttdehof@gmail.com>
2025-08-30 17:30:57 -0400

    style(config): update configuration files and import paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/filename-case */
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { lazy, Suspense } from "react";
@@ -93,7 +94,7 @@ function App() {
       </HelmetProvider>
 
       {/* React Query Devtools - only in development */}
-      {env.NODE_ENV === "development" && (
+      {env.VITE_NODE_ENV === "development" && (
         <ReactQueryDevtools
           initialIsOpen={false}
           position="right"
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./app.tsx";
+// Force TypeScript refresh
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
