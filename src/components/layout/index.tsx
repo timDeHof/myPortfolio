07e6d38154ff04b476d232aeba8e9f618862898e
timDeHof <ttdehof@gmail.com>
2025-08-30 17:38:50 -0400

    style(layout): add .tsx file extensions to import paths

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-import { ErrorBoundary } from "../common/error-boundary";
-import { Footer } from "./footer";
-import { Navigation } from "./navigation";
+import { ErrorBoundary } from "../common/error-boundary.tsx";
+import { Footer } from "./footer.tsx";
+import { Navigation } from "./navigation.tsx";
 
 export const Layout: React.FC = () => {
   return (
